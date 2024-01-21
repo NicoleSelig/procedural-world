bfaa3ca047fd01d97775d68b6e0a58a59a6e7de0
NicoleSelig <nlselig@sep.com>
2024-01-21 16:58:50 -0500

    move terrain closer to origin

--- src/Terrain.jsx
+++ src/Terrain.jsx
@@ -1,9 +1,9 @@
 import { Vector3 } from 'three'
 import Hexagon from './Hexagon.jsx'
 
-function tileToPosition(tileX, tileY) {
+function tileToPosition(tileX, tileY, terrainSize) {
     const offset = 0.5 // makes honeycomb pattern
-    return new Vector3((tileX + (tileY % 2) * offset) * 1.77, 0, tileY * 1.535)
+    return new Vector3((tileX + (tileY % 2) * offset + terrainSize/2) * 1.77, -terrainSize/2, tileY * 1.535 + terrainSize/2)
 }
 
 // // TODO: figure out how to make a circle
@@ -11,7 +11,7 @@ function makeTerrain(terrainSize = 15) {
     const hexagons = []
     for(let i=-terrainSize; i<=terrainSize, i++;) {
         for(let j=-terrainSize; j<=terrainSize, j++;) {
-            let position = tileToPosition(j, i)
+            let position = tileToPosition(j, i, terrainSize)
             hexagons.push(<Hexagon height={3} position={position} color={'purple'}/>)
         }
     }

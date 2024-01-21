1cdfb5309185e13782117ed16bd53644b573046e
NicoleSelig <nlselig@sep.com>
2024-01-21 18:39:43 -0500

    playing with noise

--- src/Hexagon.jsx
+++ src/Hexagon.jsx
@@ -2,9 +2,10 @@ import { Vector3 } from "three";
 
 export default function Hexagon({height, position, color}) {
     return(
-        <mesh 
+        <mesh
             position={position}
             key={`${position.x}-${position.z}`}
+            translateY={height/2}
         >
             <cylinderGeometry args={[1, 1, height, 6, 1, false]} />
             <meshStandardMaterial color={color} />
--- src/Terrain.jsx
+++ src/Terrain.jsx
@@ -2,20 +2,20 @@ import { Vector3 } from 'three'
 import Hexagon from './Hexagon.jsx'
 import { createNoise2D } from 'simplex-noise';
 
-function tileToPosition(tileX, tileY, terrainSize) {
+function tileToPosition(tileX, tileY, terrainSize, height) {
     const offset = 0.5 // makes honeycomb pattern
-    return new Vector3((tileX + (tileY % 2) * offset + (terrainSize/2)) * 1.77, 0, tileY * 1.535 + terrainSize/1.5)
+    return new Vector3((tileX + (tileY % 2) * offset + (terrainSize/2)) * 1.77, height/2, tileY * 1.535 + terrainSize/1.5)
 }
 
 // // TODO: figure out how to make a circle
-function makeTerrain(terrainSize = 20) {
+function makeTerrain(terrainSize = 60) {
     const hexagons = []
     for(let i=-terrainSize; i<=terrainSize, i++;) {
         for(let j=-terrainSize; j<=terrainSize, j++;) {
-            let position = tileToPosition(j, i, terrainSize)
             const noise2d = createNoise2D()
-            const noise = (noise2d(i * 0.1, j + 0.1) + 1) * 0.5
-            hexagons.push(<Hexagon height={noise * 5} position={position} color={'purple'}/>)
+            const height = (noise2d(i * 0.1, j * 0.1) + 1) * 0.5 * 5
+            let position = tileToPosition(j, i, terrainSize, height)
+            hexagons.push(<Hexagon height={height} position={position} color={'purple'}/>)
         }
     }
     return hexagons
